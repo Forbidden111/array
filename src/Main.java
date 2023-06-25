909707ff4ec95b510ccdc970e814523ee2965886
Dr.Abdulagaev <magaram.1995@mail.ru>
2023-06-25 17:57:50 +0300

    массивы сделаны

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("task1");
+        double[] array = new double[]{1.57, 7.654, 9.986};
+        double array1 = array[1];
+        System.out.println(array[0]);
+
+
+        // task2
+        System.out.println("task2");
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+
+                System.out.print(",");
+                System.out.println();
+            }
+        }
+        //task3
+        System.out.println("task3");
+        for (int i = array.length - 1; i > 0; i--) {
+            System.out.print(array[i] + ",");
+
+            System.out.print(array[i]);
+            System.out.println();
+        }
+
+
+        System.out.println("задача 4");
+        int[] weight = {1, 2, 3};
+        for (
+                int i = 0;
+                i < weight.length; i++) {
+            if (weight[i] % 2 != 0) {
+                weight[i]++;
+                System.out.println(weight[i]);
+                System.out.println();
+
+
+            }
+        }
     }
 }
+
